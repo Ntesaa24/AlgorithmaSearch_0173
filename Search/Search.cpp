511b238bc51b792b86efaeb52cd0aa7c305d43bf
Ntesaa24 <notesa.aldinasari24@gmail.com>
2024-04-04 10:50:04 +0700

    for

--- Search/Search.cpp
+++ Search/Search.cpp
@@ -5,7 +5,7 @@ int arr[20]; // Array to be seacrhed
 int n;		// Number of elements in the array 
 int i;		// Index of array element 
 
-void input() 
+void input()
 {
 	while (true)
 	{
@@ -15,9 +15,15 @@ void input()
 			break;
 		else
 			cout << "\nArray should have minimu 1 and maximum 20 elemnts.\n\n";
-	} 
+	}
 
 	// Accept array elemnts 
-	cout << "\n----------------\n"; 
-	cout << " Enter array elements \n"; 
-	cout << "-----------------\n"; 
+	cout << "\n----------------\n";
+	cout << " Enter array elements \n";
+	cout << "-----------------\n";
+	for (i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << "> ";
+		cin >> arr[i];
+	}
+}
